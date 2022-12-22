e42dc3d567dc99498957237b98a9f414c2d4c371
Nomik94 <kill0374@naver.com>
2022-12-22 09:56:35 +0900

    feat:문제 풀이 완료

---
+++ level_1/문자열_내_마음대로_정렬하기.js
@@ -0,0 +1,18 @@
+function solution(strings, n) {
+  let answer = strings.sort((x, y) => {
+    if (x[n] < y[n]) {
+      return -1;
+    } else if (x[n] > y[n]) {
+      return 1;
+    }
+    if (x[n] === y[n]) {
+      if (x < y) {
+        return -1;
+      } else if (x > y) {
+        return 1;
+      }
+    }
+    return 0;
+  });
+  return answer;
+}
